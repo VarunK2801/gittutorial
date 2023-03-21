687dd3adeeef94032bbdf588eedefd671f0bc02e
VarunK2801 <varunkapole19@gmail.com>
2023-03-21 22:17:51 +0530

    done 3rd assignment

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log("Wake up to reality! Nothing ever goes as planned in this accursed world.");
+let Title = document.getElementsByClassName('title');
+//console.log(headerTitle);
+
+console.log(Title);
+console.log(Title[0]);
+Title[0].style.fontWeight ='bold';
+Title[0].style.color='green';
+
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000'
+
