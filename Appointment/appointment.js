fb9a0c6899eda08556027cf873fa1403d51a6340
VarunK2801 <varunkapole19@gmail.com>
2023-03-24 20:51:15 +0530

    Task 12 done

--- Appointment/appointment.js
+++ Appointment/appointment.js
@@ -11,5 +11,15 @@ function submitHandler(event){
    // localStorage.setItem('email',Email);
    // localStorage.setItem('number',Number);
     const obj ={Name,Email,Number}
-    localStorage.setItem('userDetails',JSON.stringify(obj))    
+    localStorage.setItem('userDetails',JSON.stringify(obj));
+    localStorage.setItem(obj.Name, JSON.stringify(obj));
+    
+    showNewUserOnScreen(obj);
+
 }   
+function showNewUserOnScreen(user){
+    const parentNode = document.getElementById('list-of-users');
+    const childHTML = `<li> ${user.Name} - ${user.Email} - ${user.Number} </li>`
+    parentNode.innerHTML = parentNode.innerHTML +childHTML;
+
+}
