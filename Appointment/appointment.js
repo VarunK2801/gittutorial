976dc8344457bf1059ad4948ef9b74c15f112de1
VarunK2801 <varunkapole19@gmail.com>
2023-03-25 15:47:14 +0530

    Task 14 done

--- Appointment/appointment.js
+++ Appointment/appointment.js
@@ -11,7 +11,7 @@ function submitHandler(event){
    // localStorage.setItem('email',Email);
    // localStorage.setItem('number',Number);
     const obj ={Name,Email,Number}
-    localStorage.setItem('userDetails',JSON.stringify(obj));
+   // localStorage.setItem('userDetails',JSON.stringify(obj));
     localStorage.setItem(obj.Email, JSON.stringify(obj));
     
     showNewUserOnScreen(obj);
@@ -21,15 +21,19 @@ function showNewUserOnScreen(user){
     const parentNode = document.getElementById('list-of-users');
     const childHTML = `<li id= ${user.Email}> ${user.Email} - ${user.Name} - ${user.Number}
     <button onclick=deleteUser('${user.Email}') id='dbtn'> Delete User </button>
+    <button onclick=editUser('${user.Email}','${user.Name}','${user.Number}')> Edit user </button>
+    
      </li>`
 
     parentNode.innerHTML = parentNode.innerHTML +childHTML;
-
+    document.getElementById('name').value= "";
+    document.getElementById('number').value= "";
+    document.getElementById('email').value="";
 }
 
 
 function deleteUser(Email){
-    console.log(Email)
+    //console.log(Email)
     localStorage.removeItem(Email);
     removeUserFromScreen(Email);
 
@@ -40,5 +44,16 @@ function removeUserFromScreen(Email){
     const childNodeToBeDeleted = document.getElementById(Email);
 
     parentNode.removeChild(childNodeToBeDeleted)
+}
+
+function editUser(Email,Name,Number) {
+    document.getElementById('email').value=Email;
+    document.getElementById('name').value=Name;
+    document.getElementById('number').value= Number;
+   // console.log(Email,Name,Number)
+    
+
+    deleteUser(Email)
+
 }
 
